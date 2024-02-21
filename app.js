36120c9ee6ef331a1f89568c292c0464bb44be7a
ramirez-leylen <ramirez-leylen>
2024-02-21 01:35:22 -0300

    cree el archivo login.html y lo cnecte a su link

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.get("/", (req,res) => {
 app.get("/registro", (req,res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
 })
+app.get("/inicio-de-sesion", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
 
 
 app.listen(3000, () => console.log("servidor corriendo en el puerto 3000"));
